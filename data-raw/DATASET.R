3756d136a5ede12b78442db8e26fb1d36f9c0719
bobby-da-bush <robert.j.mosley1@gmail.com>
2024-11-25 10:02:29 -0500

    Init - brewR:  An all-in-one R package for managing the brewing process.

---
+++ data-raw/DATASET.R
@@ -0,0 +1,26 @@
+# install.packages("xml2")
+# install.packages("usethis")
+
+library(xml2)
+library(usethis)
+
+.url <- "http://www.beerxml.com/"
+
+.examples <- c("recipes", "hops", "grain", "misc", "style", "water", "yeast", "equipment", "mash")
+
+lapply(.examples, function(x) {
+  xml2::download_xml(
+    url = paste0(.url,x,".xml"), file = paste0("inst/extdata/",x,".xml"))
+})
+
+.files <- list.files("inst/extdata", full.names = TRUE)
+
+r_obj <- lapply(.files, xml2::read_xml)
+
+names(r_obj) <- .examples
+
+list2env(r_obj, envir = .GlobalEnv)
+
+for(obj in ls()) {
+  eval(bquote(use_data(.(as.name(obj)), overwrite = TRUE)))
+  }
